a5bd37c8a3a64143ba4c7e63b947e960cf0ed105
t-tonyo-maru <tonyomaru108@gmail.com>
2026-02-14 11:39:16 +0900

    add: files

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in pub_gem_tonyo108_hello_world.gemspec
+gemspec
+
+gem "irb"
+gem "rake", "~> 13.0"
---
+++ lib/pub_gem_tonyo108_hello_world.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pub_gem_tonyo108_hello_world/version"
+
+module PubGemTonyo108HelloWorld
+  class Error < StandardError; end
+  # Your code goes here...
+end
